9915ef4cf594058c77ebdc5a3c1c3ce2e2e48622
kmrashid <kmrownok@gmail.com>
2015-09-20 15:12:12 -0400

    ShouldWorkServiceConsumption

---
+++ ShouldWorkServiceConsumeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace ShouldWorkServiceConsumeTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(Environment.NewLine + "Retreive a WorkDayStatus by zipcode:");
+            JObject workstatus = GetWorkDayStatus(33417);
+            Console.WriteLine(workstatus);
+
+
+        }
+
+        public static JObject GetWorkDayStatus(int zipcode)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response =
+                client.GetAsync("http://localhost:52021/api/IsGoodDayToWork/IsItAGoodDayToWork/" + zipcode).Result;
+            return response.Content.ReadAsAsync<JObject>().Result;
+        }
+    }
+}
